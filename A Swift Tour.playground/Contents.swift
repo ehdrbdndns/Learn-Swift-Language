64bcb9519b1fdc2a13b1b5ed689f1e5a5fca51a1
ehdrbdndns <ehdrbdndns@naver.com>
2024-08-19 17:20:21 +0900

    Swift에서 함수와 클로저에 관한 개념 공부

--- A Swift Tour.playground/Contents.swift
+++ A Swift Tour.playground/Contents.swift
@@ -55,10 +55,83 @@ for (key, numbers) in interestingNumbers {
         }
     }
 }
+print("largest is \(largest)");
 
-print("Larget is \(largest) 😎")
-print(FlotNumber);
-print(widthTextWidthNumber);
-print("my name is \(name)");
-print(threeOfMark);
-print(totalScore);
+var total = 0
+for i in 0..<4 {
+    total += i
+}
+//print(total)
+//
+//print("Larget is \(largest) 😎")
+//print(FlotNumber);
+//print(widthTextWidthNumber);
+//print("my name is \(name)");
+//print(threeOfMark);
+//print(totalScore);
+
+// 4. Functions and Closures
+print("----Functions and Closures----")
+func greet(target: String) -> String {
+    return "Hello! \(target)"
+}
+let helloText = greet(target: "효신");
+print(helloText);
+
+func greetWithCustomArg(_ target: String, on day: String) -> String {
+    return """
+        Hello! \(target)!!! 🫣
+        Today is \(day) days
+        See you soon!!
+        """
+}
+let helloTextWithCustomArg = greetWithCustomArg("효신", on: "23");
+print(helloTextWithCustomArg);
+
+func calculateStatistics(scores: [Int]) -> (min: Int, max: Int, sum: Int) {
+    if scores.count == 0 {
+        return (min: 0, max: 0, sum: 0)
+    }
+    
+    var min = scores[0];
+    var max = scores[0];
+    var sum = 0;
+    
+    for score in scores {
+        if min > score {
+            min = score
+        }
+        
+        if max < score {
+            max = score
+        }
+        
+        sum += score
+    }
+    
+    return (min, max, sum);
+}
+let statistics = calculateStatistics(scores: [10, 0, 20, 40, 60]);
+print("The Statistic of scores is \(statistics)");
+print("The Statistic os empty scores is \(calculateStatistics(scores: []))")
+
+func makeIncrementer() -> ((Int) -> Int) {
+    func addOne(v: Int) -> Int {
+        return v + 1;
+    };
+    
+    return addOne;
+}
+let incrementer = makeIncrementer();
+print(incrementer(1))
+
+var numbers = [20, 19, 7, 12]
+let mappedNumbers = numbers.map({ (number: Int) -> Int in
+    if (number % 2) == 0 {
+        return number;
+    } else {
+        return 0;
+    }
+})
+print(mappedNumbers)
+print("------------------------------")
